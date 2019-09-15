4fb7485672a931b9fa7f0ec563b81fdb465d65c6
Fianko Buckle <fiankobuckle@gmail.com>
2019-09-15 03:48:20 -0400

    clientside.js

--- HackMITAppAzure/app.js
+++ HackMITAppAzure/app.js
@@ -16,134 +16,56 @@ const partitionKey = { kind: "Hash", paths: ["/Country"] };
 var localOrganizationName;
 var localZipcode;
 
- /**
+/**
  * Create the database if it does not exist
  */
- async function createDatabase() {
-  const { database } = await client.databases.createIfNotExists({ id: databaseId });
-  console.log(`Created database:\n${database.id}\n`);
+async function createDatabase() {
+    const { database } = await client.databases.createIfNotExists({ id: databaseId });
+    console.log(`Created database:\n${database.id}\n`);
 }
 
 /**
-* Read the database definition
-*/
+ * Read the database definition
+ */
 async function readDatabase() {
-  const { resource: databaseDefinition } = await client.database(databaseId).read();
- console.log(`Reading database:\n${databaseDefinition.id}\n`);
+    const { resource: databaseDefinition } = await client.database(databaseId).read();
+    console.log(`Reading database:\n${databaseDefinition.id}\n`);
 }
 
 /**
-* Exit the app with a prompt
-* @param {message} message - The message to display
-*/
+ * Exit the app with a prompt
+ * @param {message} message - The message to display
+ */
 function exit(message) {
-  console.log(message);
-  console.log('Press any key to exit');
-  process.stdin.setRawMode(true);
-  process.stdin.resume();
-  process.stdin.on('data', process.exit.bind(process, 0));
+    console.log(message);
+    console.log('Press any key to exit');
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
 }
 
 createDatabase()
-  .then(() => readDatabase())
-  .then(() => createContainer())
-  .then(() => readContainer())
-  .then(() => createOrganization(config.items.Org1))
-  .then(() => { exit(`Completed successfully`); })
-  .catch((error) => { exit(`Completed with error ${JSON.stringify(error) }`) });
+    .then(() => readDatabase())
+    .then(() => createContainer())
+    .then(() => readContainer())
+    .then(() => createOrganization(config.items.Org1))
+    .then(() => { exit(`Completed successfully`); })
+    .catch((error) => { exit(`Completed with error ${JSON.stringify(error) }`) });
 
-  /**
-* Create the container if it does not exist
-*/
+/**
+ * Create the container if it does not exist
+ */
 
 async function createContainer() {
 
     const { container } = await client.database(databaseId).containers.createIfNotExists({ id: containerId, partitionKey }, { offerThroughput: 400 });
     console.log(`Created container:\n${config.container.id}\n`);
-   }
-   
-   /**
-    * Read the container definition
-   */
-   async function readContainer() {
-      const { resource: containerDefinition } = await client.database(databaseId).container(containerId).read();
+}
+
+/**
+ * Read the container definition
+ */
+async function readContainer() {
+    const { resource: containerDefinition } = await client.database(databaseId).container(containerId).read();
     console.log(`Reading container:\n${containerDefinition.id}\n`);
-   }
-
-/**
- * Action to occur when user inputs their organization name and clicks Event List
- */
-$("#to-event-list-btn").click (
-    
-    function () {
-                    
-        createOrganization();        
-        
-    }
-
-);
-
-/**
- * Action to occur when user inputs their organization name and clicks New Event
- */
-$("#new-event-btn").click (
-    
-    function () {
-                    
-        createOrganization();        
-    }
-
-);
-/**
- * Action to occur when organization makes new event
- */
-$("ad-entry-btn").click (
-    function () {
-        
-        var postID = guid();
-        createPost(postID, localOrganizationName);        
-
-    }
-);
-
-/**
- * Action to occur when volunteer enters zipcode
- */
-$("zipcode-entry-button").click (
-    function () {
-        
-        localZipcode = $("#zipcode-input").val ();
-
-    }
-);
-
-async function createOrganization (organizationJson) {
-        
-    //for ()
-    //localOrganizationName = $("#org-name").val ();
-    //var name = localOrganizationName;    
-    const { item } = await client.database(databaseId).container(containerId).items.upsert(organizationJson);
-}
-
-async function createPost (postID, organizationName) {
-
-    var orgName = organizationName;
-    var eventName = $("#event-name").val ();
-    var eventDescription = $("#event-description").val ();
-    var eventDate = $("#event-date").val ();
-    var eventAddress = $("#event-address").val ();
-    var volunteersNeeded = $("#event-volunteers").val ();
-    var volunteersHad = 0;
-    const { item } = await client.database(databaseId).container(containerId).items.upsert(itemBody);
-
-}
-
-function guid () {
-        
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
-    
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-    
 }
